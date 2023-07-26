cb20b689afc6ecb2224d8b5d3507a96c15dcb030
Nick Thorpe <nickthorpe71@gmail.com>
2023-07-26 10:41:16 -0500

    fix page title

--- src/app/(main-menu)/main-menu/requests/page.tsx
+++ src/app/(main-menu)/main-menu/requests/page.tsx
@@ -40,7 +40,7 @@ const RequestPage = async () => {
 
     return (
         <main className='pt-8'>
-            <h1 className='font-bold text-5xl mb-8'>Add a friend</h1>
+            <h1 className='font-bold text-5xl mb-8'>Friend Requests</h1>
             <div className='flex flex-col gap-4'>
                 <FriendRequests
                     incomingFriendRequests={incomingFriendRequests}
