9625bdbd82f008aad5c5677b41c67265a942043b
Nick Thorpe <nickthorpe71@gmail.com>
2023-07-26 19:36:16 -0500

    fix base page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
-import Button from "@/components/ui/Button";
-import { db } from "@/lib/db";
+"use client";
 
-export default async function Home() {
-    return <Button>Test</Button>;
+import Button from "@/components/ui/Button";
+import { signOut } from "next-auth/react";
+
+export default function Home() {
+    return <Button onClick={() => signOut()}>Sign out</Button>;
 }
