34ea8cd2d16c74052d116963d213474937df94c2
Nick Thorpe <nickthorpe71@gmail.com>
2023-07-26 19:36:27 -0500

    add logo to login

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,9 +1,14 @@
 "use client";
 
-import Button from "@/components/ui/Button";
 import { FC, useState } from "react";
 import { signIn } from "next-auth/react";
 import { toast } from "react-hot-toast";
+
+// lib
+import { Icons } from "@/components/Icons";
+
+// components
+import Button from "@/components/ui/Button";
 
 const Page: FC = () => {
     const [isLoading, setIsLoading] = useState<boolean>(false);
@@ -24,7 +29,7 @@ const Page: FC = () => {
             <div className='flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8'>
                 <div className='w-full flex flex-col items-center max-w-md space-y-8'>
                     <div className='flex flex-col items-center gap-8'>
-                        logo
+                        <Icons.Logo />
                         <h2 className='mt-6 text-center text-3xl font-bold tracking-tight text-gray-900'>
                             Sign in to your account
                         </h2>
